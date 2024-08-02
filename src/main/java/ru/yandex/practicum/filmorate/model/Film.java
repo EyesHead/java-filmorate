5b49b1dfdfd571365995463c83d5d9cac4802de0
Даниил Куксарь <dan.kukar@yandex.ru>
2024-08-02 15:19:38 +0300

    Film model fields were filled in

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,10 +3,18 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Duration;
+import java.time.Instant;
+
 /**
  * Film.
  */
 @Getter
 @Setter
 public class Film {
+    Integer id;
+    String name;
+    String description;
+    Instant releaseDate;
+    Duration duration;
 }
