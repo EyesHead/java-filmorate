4e37a2375a9c51373f717d6eca1a8e25acb682aa
Даниил Куксарь <dan.kukar@yandex.ru>
2024-08-04 03:30:23 +0300

    checkstyle: unused imports removed

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,13 +3,9 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(FilmorateApplication.class, args);
     }
 }
