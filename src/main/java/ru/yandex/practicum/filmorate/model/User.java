f664deea4ba88bf63d09a9fa49ad3b0a9da3e38c
Даниил Куксарь <dan.kukar@yandex.ru>
2024-08-04 03:43:46 +0300

    checkstyle: unused import delete

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
