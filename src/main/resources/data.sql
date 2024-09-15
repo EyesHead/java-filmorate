fc177329e6e6374520c0c79944f6f6a1b102811b
Даниил Куксарь <dan.kukar@yandex.ru>
2024-09-15 20:09:42 +0300

    Fix: clean mpa and genres tables before initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,22 +19,18 @@ ALTER TABLE films_genres ALTER COLUMN genre_id RESTART WITH 1;
 
 ALTER TABLE films ALTER COLUMN film_id RESTART WITH 1;
 ALTER TABLE users ALTER COLUMN user_id RESTART WITH 1;
-ALTER TABLE genres ALTER COLUMN genre_id RESTART WITH 1;
 ALTER TABLE mpa ALTER COLUMN mpa_id RESTART WITH 1;
+ALTER TABLE genres ALTER COLUMN genre_id RESTART WITH 1;
 
--- Вставка данных в таблицу mpa
-INSERT INTO mpa (mpa_id, mpa_name) VALUES
-                                       (1, 'G'),
-                                       (2, 'PG'),
-                                       (3, 'PG-13'),
-                                       (4, 'R'),
-                                       (5, 'NC-17');
+INSERT INTO mpa VALUES (1, 'G'),
+                       (2, 'PG'),
+                       (3, 'PG-13'),
+                       (4, 'R'),
+                       (5, 'NC-17');
 
--- Вставка данных в таблицу genres
-INSERT INTO genres (genre_id, name) VALUES
-                                        (1, 'Комедия'),
-                                        (2, 'Драма'),
-                                        (3, 'Мультфильм'),
-                                        (4, 'Триллер'),
-                                        (5, 'Документальный'),
-                                        (6, 'Боевик');
+INSERT INTO genres VALUES (1, 'Комедия'),
+                          (2, 'Драма'),
+                          (3, 'Мультфильм'),
+                          (4, 'Триллер'),
+                          (5, 'Документальный'),
+                          (6, 'Боевик');
