5cd068b7b73507d6bff22a1fd421339b82f315ec
MihalinaGotovchits <Mix-Mihalina@ya.ru>
2024-09-29 01:48:55 +0300

    add remove endpoint

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserCrudController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserCrudController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.entity.Film;
 import ru.yandex.practicum.filmorate.entity.Marker;
 import ru.yandex.practicum.filmorate.entity.User;
 import ru.yandex.practicum.filmorate.service.UserCrudService;
